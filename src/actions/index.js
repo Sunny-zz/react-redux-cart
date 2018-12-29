fff86b2491f9719c0a6b230ef02e44ffe0bd5949
Sunny-zz <1348369672@qq.com>
2018-12-29 10:48:04 +0800

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,26 +3,49 @@ import * as actionTypes from '../constants'
 // * as actionTypes 将 constants 内导出的所有东西全部放到 actionTypes 对象内
 console.log(actionTypes)
 
+// export const getProducts = () => {
+//   return dispatch => {
+//     axios.get('http://localhost:3008/products').then(res => {
+//       dispatch({
+//         type: actionTypes.GET_PRODUCTS,
+//         products: res.data
+//       })
+//     })
+//   }
+// }
+
 export const getProducts = () => {
   return dispatch => {
-    axios.get('http://localhost:3008/products').then(res => {
-      dispatch({
-        type: actionTypes.GET_PRODUCTS,
-        products: res.data
-      })
-    })
-  }
-}
-export const addToCart = (id, newInventory) => {
-  return dispatch => {
     axios
-      .patch(`http://localhost:3008/products/${id}`, newInventory)
-      .then(() => {
+      .get(
+        'https://raw.githubusercontent.com/Sunny-zz/react-redux-cart/master/api/db.json'
+      )
+      .then(res => {
         dispatch({
-          type: actionTypes.ADD_TO_CART,
-          id
+          type: actionTypes.GET_PRODUCTS,
+          products: res.data.products
         })
       })
+  }
+}
+
+// export const addToCart = (id, newInventory) => {
+//   return dispatch => {
+//     axios
+//       .patch(`http://localhost:3008/products/${id}`, newInventory)
+//       .then(() => {
+//         dispatch({
+//           type: actionTypes.ADD_TO_CART,
+//           id
+//         })
+//       })
+//   }
+// }
+
+export const addToCart = id => {
+  return {
+    type: actionTypes.ADD_TO_CART,
+    id
   }
 }
 
@@ -32,15 +55,21 @@ export const checkoutCart = () => {
   }
 }
 
-export const subProductQuantity = (id, newInventory) => {
-  return dispatch => {
-    axios
-      .patch(`http://localhost:3008/products/${id}`, newInventory)
-      .then(() => {
-        dispatch({
-          type: actionTypes.SUB_PRODUCT_QUANTITY,
-          id
-        })
-      })
+// export const subProductQuantity = (id, newInventory) => {
+//   return dispatch => {
+//     axios
+//       .patch(`http://localhost:3008/products/${id}`, newInventory)
+//       .then(() => {
+//         dispatch({
+//           type: actionTypes.SUB_PRODUCT_QUANTITY,
+//           id
+//         })
+//       })
+//   }
+// }
+export const subProductQuantity = id => {
+  return {
+    type: actionTypes.SUB_PRODUCT_QUANTITY,
+    id
   }
 }
